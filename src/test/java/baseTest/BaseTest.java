505a7d03a3505c9130d54ff6ce21575ee9247182
Olya <olhavasylivnaqa@gmail.com>
2023-05-10 21:16:37 +0300

    added Allure. Test Case: add item to basket,  test case Filter_TC

--- src/test/java/baseTest/BaseTest.java
+++ src/test/java/baseTest/BaseTest.java
@@ -6,6 +6,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pages.HomePage;
 
 import java.time.Duration;
 
@@ -14,19 +16,41 @@ public class BaseTest {
     protected WebDriver webDriver;
     Logger logger = Logger.getLogger(getClass());
 
+    protected HomePage homePage;
+
     @Before
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
+
+
+        webDriver = initDriver();
         webDriver.manage().window().maximize();
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+
+        homePage = new HomePage(webDriver);
+
 
     }
 
     @After
     public void quitBrowser() {
         webDriver.quit();
-
+        logger.info("was closed browser");
     }
 
+
+    private WebDriver initDriver() {
+
+        String browser = System.getProperty("browser");
+        if ((browser == null) || "chrome".equalsIgnoreCase(browser)) {
+
+
+            ChromeOptions ops = new ChromeOptions();
+            ops.addArguments("--remote-allow-origins=*");
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver(ops);
+
+        }
+        return webDriver;
+    }
 }
